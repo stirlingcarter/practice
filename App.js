becd31258a857bb3dd2cd16cfab02cc4036e90e1
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2020-04-27 12:39:55 -0400

    added screens

--- App.js
+++ App.js
@@ -6,21 +6,41 @@ import AssetExample from './components/AssetExample';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 // STYLE IMPORT
 import { app_styles } from "./styles/styles.js";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={app_styles.container}>
-      <Text style={app_styles.paragraph}>
-        Change code in the editor and watch it changedd on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+ 
---
+++ components/Instrument.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+
+// STYLE IMPORT
+import {instrument_styles, router_demo_styles} from "../styles/styles.js";
+
+
+export default function AssetExample() {
+  return (
+    <View >
+      <Text>
+        PIANO
+      </Text>
+    </View>
+  );
+}
+
+
