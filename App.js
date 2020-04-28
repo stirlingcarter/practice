2035bfe1ec41a453558092d094501c3faa9be3ed
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2020-04-27 21:26:52 -0400

    prettify

--- App.js
+++ App.js
@@ -1,50 +1,59 @@
-import * as React from 'react';
+import * as React from "react";
 
-
-import { FlatList, Button, Text, View, StyleSheet } from 'react-native';
+import { FlatList, Button, Text, View, StyleSheet } from "react-native";
 
 // You can import from local files
-import AssetExample from './components/AssetExample';
+import AssetExample from "./components/AssetExample";
 
 // or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { Card } from "react-native-paper";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 // STYLE IMPORT
 import { app_styles } from "./styles/styles.js";
 
-
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options = {{title : "HOME_SCR"} }/>
-        <Stack.Screen name="InstrumentScreen" component={InstrumentScreen} options = {{title : "INSTR_SCR"}} />
-        <Stack.Screen name="LessonScreen" component={LessonScreen} options = {{title : "lesson_screen_title"}} />
-
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "HOME_SCR" }}
+        />
+        <Stack.Screen
+          name="InstrumentScreen"
+          component={InstrumentScreen}
+          options={{ title: "INSTR_SCRj" }}
+        />
+        <Stack.Screen
+          name="LessonScreen"
+          component={LessonScreen}
+          options={{ title: "lesson_screen_title" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 function HomeScreen({ navigation }) {
-
-  var instrumentNames = getInstrumentNames()
+  var instrumentNames = getInstrumentNames();
 
   return (
     <View style={styles1.container}>
       <FlatList
-        data = {instrumentNames}
-        renderItem={({item}) =>       
-        <Button
-          title= {item}
-          onPress={() => navigation.navigate("InstrumentScreen", { instrument : item})}
-        />}
+        data={instrumentNames}
+        renderItem={({ item }) => (
+          <Button
+            title={item}
+            onPress={() =>
+              navigation.navigate("InstrumentScreen", { instrument: item })
+            }
+          />
+        )}
       />
     </View>
   );
@@ -53,30 +62,37 @@ function HomeScreen({ navigation }) {
 function InstrumentScreen({ route, navigation }) {
   const { instrument } = route.params;
 
-  var options = ["stats", "lessons"]
+  var lessons = ["lesson1", "lesson2", "3"];
+
   return (
     <View style={styles1.container}>
-            <Text>{instrument}</Text>
-      <FlatList
-        data = {options} 
-        renderItem={({item}) =>       
-        <Button
-          title= {item}
-          onPress={() => navigation.navigate({item})}
-        />}
-      />
+      <Text>hi</Text>
+      <LessonContainer nav={navigation} lessons={lessons} />
     </View>
   );
-  }
-
-function getInstrumentNames(){
-  var names = ["guitar", "piano"]
-  return names
 }
 
-function LessonScreen() {
+function getInstrumentNames() {
+  var names = ["guitar", "piano"];
+  return names;
+}
+
+function LessonContainer(props) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <FlatList
+      data={props.lessons}
+      renderItem={({ item }) => (
+        <Button title={item} onPress={() => props.nav.navigate({ item })} />
+      )}
+    />
+  );
+}
+
+function LessonScreen({ route, navigation }) {
+  const { lesson } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>playcard component go here</Text>
     </View>
   );
@@ -84,8 +100,8 @@ function LessonScreen() {
 
 const styles1 = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    paddingTop: 22,
   },
   item: {
     padding: 10,
--- components/Instrument.js
+++ components/Instrument.js
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import * as React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { NativeRouter, Route, Link } from "react-router-native";
 
 // STYLE IMPORT
-import {instrument_styles, router_demo_styles} from "../styles/styles.js";
-
+import { instrument_styles, router_demo_styles } from "../styles/styles.js";
 
 export default function AssetExample() {
   return (
-    <View >
-      <Text>
-        PIANO
-      </Text>
+    <View>
+      <Text>PIANO</Text>
     </View>
   );
 }
-
-
