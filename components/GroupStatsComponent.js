44c64e4d8d87f221846a6c07e5a7a0543fee82c9
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2022-11-20 17:58:57 -0800

    planned out groupstats everything broken

--- components/GroupStatsComponent.js
+++ components/GroupStatsComponent.js
@@ -3,18 +3,18 @@ import {
   Text,
   View
 } from "react-native";
-import challengeService from "../App"
+import { lessonRepository, statService } from "../App"
 import { ScrollView } from "react-native-gesture-handler";
 import { LessonCategoryRadarChartComponent } from "../components/LessonCategoryRadarChartComponent";
 import { LessonCategoryLineChartComponent } from "../components/LessonCategoryLineChartComponent";
 
-const DOMAIN = {y:[0,100]}
+const DOMAIN = { y: [0, 100] }
 const LIMIT = 30 * 1000
 
 export class GroupStatsComponent extends React.Component {
 
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
     };
   }
@@ -33,8 +33,209 @@ export class GroupStatsComponent extends React.Component {
   componentDidUpdate() {
   }
 
+  f(x) {
+
+    let goal = this.props.lesson.getGoal() * 1000
+
+    //approach 100 as x approaches  0
+    //approach 0 as x approaches inf  
+    if (x == 0 || x > LIMIT) {
+      return 10
+    } if (x <= goal) {
+      return 100
+    }
+    let diffFromGoal = (x - goal) / 1000
+    let ret = 100 - (16.4317 * Math.sqrt(diffFromGoal))
+    return (ret > 0 ? ret : 10)
+  }
 
   render() {
-    return (<View><Text>not impled yet hunney</Text></View>)
+
+    //1. weakest variants 
+    let lessonNames = groupRepository.getGroupByPath(Path.currentDirectory(this.props.path)).getLessonNames()
+
+    let recentAvgsByLessonName = {}
+    let originalAvgsByLessonName = {}
+    let lessons = lessonNames.map(name => {
+      lessonRepository.getLessonByPath(Path.Plus(this.props.path, name))
+    })
+    for (lesson of lessons) {
+
+
+
+      /*
+  returns -> 
+      { "lesson1" : 
+        [[[2,6,3,6,4,7,6,4,8,2,6,7],
+        [5,2,6,7,3],
+        [1,6]],
+        [[a,b,c....g],
+        [maj7,m7...d7],
+        [left,right]]],...
+      }
+*/
+      recentAvgsByLessonName[name] = statsService.getRecentAveragesByVariant(lesson)
+      /*
+  returns -> 
+      { "lesson1" : 
+        [[[2,6,3,6,4,7,6,4,8,2,6,7],
+        [5,2,6,7,3],
+        [1,6]],
+        [[a,b,c....g],
+        [maj7,m7...d7],
+        [left,right]]],...
+      }
+*/
+      originalAvgsByLessonName[name] = statsService.getOriginalAveragesByVariant(lesson)
+    }
+
+    /*
+    still returns -> 
+    { "lesson1" : 
+        [[[100,600,300,60,40,70,60,40,80,200,60,70],
+        [50,200,60,70,300],
+        [100,60]],
+        [[a,b,c....g],
+        [maj7,m7...d7],
+        [left,right]]],...
+    }
+*/
+    let lessonNameToPercentsImproved = {}
+    for (lesson of lessons) {
+      lessonNameToPercentImproved[lesson.getName()] = getPercentImproved(originalAvgsByLessonName[lesson.getName()], recentAvgsByLessonName[lesson.getName()])
+    }
+
+
+    /*
+just returns -> 
+ 
+    [[[100,600,300,60,40,70,60,40,80,200,60,70],
+    [50,200,60,70,300],
+    [100,60]],
+    [[a,b,c....g],
+    [maj7,m7...d7],
+    [left,right]]],...
+    ***HURR***
+*/
+    let variantToOverallAvgImprovement = this.getOverallAvgImprovementByVariant(lessonNameToPercentsImproved)
+
+
+        /*
+    returns -> 
+ 
+    [[[1,6,3,6,4,7,6,4,8,2,6,7],
+    [5,2,6,7,3],
+    [1,6]],
+    [[a,b,c....g],
+    [maj7,m7...d7],
+    [left,right]]],...
+    ***HURR***
+*/
+    let variantToOverallAvgTime = this.getVariantToOverallAvgTime(Util.copyOf(lessonNameToPercentsImproved))
+
+    let slowestVariants = this.getReduceEachListToMax(Util.copyOf(variantToOverallAvgTime))
+    let fastestVariants = this.getReduceEachListToMin(Util.copyOf(variantToOverallAvgTime))
+
+    let lessonNamesMapToImprovements = Object.keys(lessonNameToPercentImproved)
+    let lessonImprovements = lessonNamesMapToImprovements.map(key => {
+        getSingularLessonAvg(lessonNameToPercentsImproved[key])
+    })
+    let maxIndex = 0;
+    let max = -13876;
+    for (let i = 0; i < lessonImprovements.length; i++){
+      let thisLessonImproved = lessonImprovements[i]
+      if (thisLessonImproved > max){
+        max = thisLessonImproved
+        maxIndex = i
+      }
+    }
+    let mostImprovedLessonName = lessonNamesMapToImprovements[maxIndex]
+    let mostImprovedLessonImprovement = lessonImprovements[maxIndex]
+
+    
+    let variantsWithMostImprovementOverall = this.getReduceEachListToMax(Util.copyOf(variantToOverallAvgImprovement))
+    /*
+    Lesson with most improvement overall(1) -> mostImprovedLessonName CHECK
+      - list - % improvement by lesson(no#lessons) -> vars lessonNamesMapToImprovements, lessonImprovements CHECK
+
+    Variant with most improvement overall(3) -> variantsWithMostImprovementOverall CHECK
+      - list - % improvement by variant(no#variants)(3) -> variantToOverallAvgImprovement CHECK
+    Strongest Variant(3) - slowestVariants CHECK
+    Weakest Variant(3) - fastestVariants CHECK
+    
+    */ 
+
+
+
+
+
+
+
+
+
+    //get historical averages for each variant we see in this group
+    //1. get a list of all unique vartiants in the group 
+    //2. now, using those names as keys, create dict mapping them to constructed times array for their whole history
+    // a big task of course d
+    //3. now you can get a window from there to create a radial chart or the whole thing to make a line chart 
+    //  - for each category of variant reporesented. 
+
+    let response = statService.getRecentAveragesByVariant(this.props.lesson)
+    let averagesByVariant = response[0]
+    let namesOfVariants = response[1]
+    let adjustedAveragesByVariant = []
+    for (let i = 0; i < averagesByVariant.length; i++) {
+      let adjustedList = []
+      for (let k = 0; k < averagesByVariant[i].length; k++) {
+        adjustedList.push(this.f(averagesByVariant[i][k]))
+      }
+      adjustedAveragesByVariant.push(adjustedList)
+    }
+    if (adjustedAveragesByVariant.length == 1) {
+      return (
+        <ScrollView>
+
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[0]} namesOfVariants={namesOfVariants[0]} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[0]} lesson={this.props.lesson} />
+
+        </ScrollView>
+
+      );
+    } else if (adjustedAveragesByVariant.length == 2) {
+
+
+      return (
+        <ScrollView>
+
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[0]} namesOfVariants={namesOfVariants[0]} />
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[1]} namesOfVariants={namesOfVariants[1]} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[0]} lesson={this.props.lesson} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[1]} lesson={this.props.lesson} />
+
+        </ScrollView>
+
+
+      );
+    } else if (adjustedAveragesByVariant.length == 3) {
+
+      return (
+
+        <ScrollView>
+
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[0]} namesOfVariants={namesOfVariants[0]} />
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[1]} namesOfVariants={namesOfVariants[1]} />
+          <LessonCategoryRadarChartComponent averages={adjustedAveragesByVariant[2]} namesOfVariants={namesOfVariants[2]} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[0]} lesson={this.props.lesson} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[1]} lesson={this.props.lesson} />
+          <LessonCategoryLineChartComponent namesOfVariants={namesOfVariants[2]} lesson={this.props.lesson} />
+
+
+        </ScrollView>
+
+
+      );
+    } else {
+      return (<View><Text>huh?</Text></View>)
+    }
   }
 }
